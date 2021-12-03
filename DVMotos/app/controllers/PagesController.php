24bc776c1f6e8accbcfb42b0f6d4a4c0be215e88
marialuisarg <marialuisariolinoguimaraes@gmail.com>
2021-12-02 23:04:46 -0300

    fazendo rotas (não sei se tá certo mas funcionou)

--- DVMotos/app/controllers/PagesController.php
+++ DVMotos/app/controllers/PagesController.php
@@ -12,6 +12,55 @@ class PagesController
         return view('/site/home');
     }
 
+    public function quemsomos()
+    {
+        return view('/site/quemsomos');
+    }
+
+    public function contato()
+    {
+        return view('/site/contato');
+    }
+
+    public function login()
+    {
+        return view('/site/login');
+    }
+
+    public function produtos()
+    {
+        return view('/site/produtos');
+    }
+
+    public function produto()
+    {
+        return view('/site/produto');
+    }
+
+
+
+    public function admDashboard()
+    {
+        return view('/admin/adm-dashboard');
+    }
+
+    public function admCategorias()
+    {
+        return view('/admin/adm-categorias');
+    }
+
+    public function admProdutos()
+    {
+        return view('/admin/adm-produtos');
+    }
+
+    public function admUser()
+    {
+        return view('/admin/adm-user');
+    }
+
+
+    
     public function show()
     {
         
--- DVMotos/app/routes.php
+++ DVMotos/app/routes.php
@@ -3,3 +3,13 @@
 use App\Controllers\PagesController;
 
 $router->get('','PagesController@index');
+$router->get('quem-somos', 'PagesController@quemsomos');
+$router->get('contato', 'PagesController@contato');
+$router->get('login', 'PagesController@login');
+$router->get('produtos', 'PagesController@produtos');
+$router->get('produto', 'PagesController@produto');
+
+$router->get('adm-dashboard', 'PagesController@admDashboard');
+$router->get('adm-usuarios', 'PagesController@admUser');
+$router->get('adm-categorias', 'PagesController@admCategorias');
+$router->get('adm-produtos', 'PagesController@admProdutos');
